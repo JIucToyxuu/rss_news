47fd42ebf1d910ef0e80234b1b31f937b78703a1
JIucToyxuu <antonduzhnov.90@gmail.com>
2011-07-11 09:51:01 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssNews::Application.config.secret_token = 'ec3c7884d114c2f76048ff8c07b980ef63aa182c56758d7d037e3f714d4640c504cc521bbfe73bca6f8dc305ab7e696e24b532d9cabf0c3865d65db7a701497d'
