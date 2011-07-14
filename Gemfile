fed2bdbe5f2fa957b708aecdd6334db412d77627
JIucToyxuu <antonduzhnov.90@gmail.com>
2011-07-14 09:55:18 +0400

    Save norm no double

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
-
+gem 'ffaker', '1.8.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,5 +1,28 @@
 class RssController < ApplicationController
-  def rss_show
-    @lenta= RssReader('http://world-pulse.ru/feed')
-  end
+   def
+    read_rss
+      require 'simple-rss'
+      require 'open-uri'
+      feed_items = SimpleRSS.parse(open('http://world-pulse.ru/feed')).items
+	#@lenta = feed_items
+	#@lenta.echo do |item|
+
+# <% @lenta.each do |item| -%>
+ # <%= h item.title %>
+ # < %= h item.description %>
+# < %= h item.author %>
+ # < %= link_to('Date:',item.pubdate %>
+ #< %= link_to('Url:',item.link) %>
+ #<% end -%>
+newss = {}
+feed_items.echo do |item|
+@name = Faker::Name.first_name
+ newss[@name] = News.new
+newss[@name][:title] = item.title
+newss[@name][:link] = item.link
+newss[@name][:description] = item.description
+ newss[@name].save
+
+    end
+  end 
 end
